8be8ff59a34d8ad65c0dac427607b1d77d27a9fc
Lynn De Souza <lynn.desouza@alumni.uct.ac.za>
2018-08-22 16:16:33 +0100

    working mvc

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_111844) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "phone"
-    t.string "number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the DB'
+Restaurant.destroy_all
+
+puts 'Creating Data'
+
+cafe_a = Restaurant.create(name: 'Cafe A',
+  address: '123 Drive', category: 'belgian',
+  phone_number: '0700111222')
+
+cafe_b = Restaurant.create(name: 'Cafe B',
+  address: '345 Drive', category: 'french',
+  phone_number: '0700111222')
+
+cafe_c = Restaurant.create(name: 'Cafe C',
+  address: '567 Drive', category: 'japanese',
+  phone_number: '0700111222')
+
+cafe_d = Restaurant.create(name: 'Cafe D',
+  address: '890 Drive', category: 'chinese',
+  phone_number: '0700111222')
+
+cafe_e = Restaurant.create(name: 'Cafe E',
+  address: '000 Drive', category: 'japanese',
+  phone_number: '0700111222')
+
+cafe_f = Restaurant.create(name: 'Cafe F',
+  address: '111 Drive', category: 'italian',
+  phone_number: '0700111222')
+
+puts 'Finished!'
